709920a88b911da9fc7344c5e3730f59dbbb7001
Jacob Curley <jcurley@oxy.edu>
2021-02-24 10:48:23 -0600

    Changes to detect user connections and disconnects

--- TestServer/server.py
+++ TestServer/server.py
@@ -1,15 +1,9 @@
-from enum import Enum
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import emit, SocketIO
 
 server = Flask(__name__)
 server.config['SECRET KEY'] = 'svonwoudnwvob1235#'
 socketio = SocketIO(server)
-
-
-class Methods(Enum):
-    GET = 'GET'
-    POST = 'POST'
 
 
 @server.route('/')
@@ -17,15 +11,27 @@ def sessions():
     return render_template('index.html')
 
 
-def message_received(methods=Methods):
+@socketio.on('connect')
+def connect():
+    print('User connected')
+    emit('my response', {'user_name': 'ANNOUNCEMENT', 'message': 'User connected'})
+
+
+@socketio.on('disconnect')
+def disconnect():
+    print('User disconnected')
+    emit('my response', {'user_name': 'ANNOUNCEMENT', 'message': 'User disconnected'})
+
+
+def message_received():
     print('message received')
 
 
 @socketio.on('my event')
-def handle_custom_event(json, methods=Methods):
+def handle_custom_event(json):
     print('received event: ' + str(json))
     socketio.emit('my response', json, callback=message_received)
 
 
 if __name__ == '__main__':
-    socketio.run(server, host="0.0.0.0", port=8080, debug=True)
+    socketio.run(server, host="0.0.0.0", port=8080)
